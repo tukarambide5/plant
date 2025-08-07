cbe92597c311e2b710103287e4d4e7676d7c710b
T <tukarambide5@gmail.com>
2025-08-07 07:08:04 +0000

    make it pwa and use premadeicon as app icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from '@/components/leafwise/theme-provider';
 export const metadata: Metadata = {
   title: 'LeafWise',
   description: 'Identify plants and get care guides with a snapshot.',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -19,6 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400..900&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#4A6C53" />
       </head>
       <body className="font-body antialiased">
         <ThemeProvider
